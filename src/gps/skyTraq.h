14a7311c7b40b52653bf4ff3e036d3febeac0f83
Ian McEwan <ijm@ashimaarts.com>
2013-11-04 18:49:36 -0800

    updating skytraq autogen include with packed structs.

--- src/gps/skyTraq.h
+++ src/gps/skyTraq.h
@@ -3,6 +3,8 @@
    Copywrite 2013 Ashima Research
  */
 
+#define PACKED __attribute__((__packed__))
+
       
   
 enum message_ids_t { 
@@ -44,10 +46,10 @@ enum message_ids_t {
     gps_position_pinning_status = 0xB4, /* Position pinning status of the GPS receiver */
     gps_navigation_mode = 0xB5, /* Navigation mode of the GPS receiver */
     gps_1pps_mode = 0xB6, /* 1PPS mode of GPS receiver */
-   }
+   };
   
-;  
-typedef struct message_0x01_t { 
+  
+typedef struct PACKED message_0x01_t { 
     uint8_t  message_id ; /*  */
     uint8_t  start_mode ; /* 00 = Reserved 01 = System Reset, Hot start 02 = System Reset, Warm start 03 = System Reset, Cold start 04 = Reserved */
     uint16_t  utc_year ; /* >= 1980 */
@@ -62,25 +64,25 @@ typedef struct message_0x01_t {
    } message_0x01_t;
   
   
-typedef struct message_0x02_t { 
+typedef struct PACKED message_0x02_t { 
     uint8_t  message_id ; /*  */
     uint8_t  software_type ; /* 00 = Reserved 01 = System code */
    } message_0x02_t;
   
   
-typedef struct message_0x03_t { 
+typedef struct PACKED message_0x03_t { 
     uint8_t  message_id ; /*  */
     uint8_t  software_type ; /* 00 = Reserved 01 = System code */
    } message_0x03_t;
   
   
-typedef struct message_0x04_t { 
+typedef struct PACKED message_0x04_t { 
     uint8_t  message_id ; /*  */
     uint8_t  type ; /* 00 = Reserved 01 = reboot after setting to factory defaults */
    } message_0x04_t;
   
   
-typedef struct message_0x05_t { 
+typedef struct PACKED message_0x05_t { 
     uint8_t  message_id ; /*  */
     uint8_t  com_port ; /* 00 = COM 1 */
     uint8_t  baud_rate ; /* 0: 4800 1: 9600 2: 19200 3: 38400 4: 57600 5: 115200 */
@@ -88,7 +90,7 @@ typedef struct message_0x05_t {
    } message_0x05_t;
   
   
-typedef struct message_0x08_t { 
+typedef struct PACKED message_0x08_t { 
     uint8_t  message_id ; /*  */
     uint8_t  gga_interval ; /* 0 ~255, 0: disable */
     uint8_t  gsa_interval ; /* 0 ~255, 0: disable */
@@ -101,40 +103,40 @@ typedef struct message_0x08_t {
    } message_0x08_t;
   
   
-typedef struct message_0x09_t { 
+typedef struct PACKED message_0x09_t { 
     uint8_t  message_id ; /*  */
     uint8_t  type ; /* 00 : No output 01 : NMEA message 02 : Binary Message */
     uint8_t  attributes ; /* 0: update to SRAM 1: update to both SRAM & FLASH */
    } message_0x09_t;
   
   
-typedef struct message_0x0C_t { 
+typedef struct PACKED message_0x0C_t { 
     uint8_t  message_id ; /*  */
     uint8_t  mode ; /* 00 = Normal (disable) 01 = Power Save (enable) */
     uint8_t  attributes ; /* 0: update to SRAM 1: update to both SRAM & FLASH 2: temporarily enabled */
    } message_0x0C_t;
   
   
-typedef struct message_0x0E_t { 
+typedef struct PACKED message_0x0E_t { 
     uint8_t  message_id ; /*  */
     uint8_t  rate ; /* Value with 1, 2, 4, 5, 8 or 10 01: 1Hz update rate Note: value with 4 or higher should work with baud rate 38400 or higher */
     uint8_t  attributes ; /* 0: update to SRAM 1: update to both SRAM & FLASH */
    } message_0x0E_t;
   
   
-typedef struct message_0x10_t { 
+typedef struct PACKED message_0x10_t { 
     uint8_t  message_id ; /*  */
    } message_0x10_t;
   
   
-typedef struct message_0x11_t { 
+typedef struct PACKED message_0x11_t { 
     uint8_t  message_id ; /*  */
     uint8_t  navigation_message_interval ; /* 0 ~255, 0: disable */
     uint8_t  attributes ; /* 0: update to SRAM 1: update to both SRAM & FLASH */
    } message_0x11_t;
   
   
-typedef struct message_0x29_t { 
+typedef struct PACKED message_0x29_t { 
     uint8_t  message_id ; /*  */
     uint16_t  index ; /* Refer to Appendix B for available Datum */
     uint8_t  ellip_idx ; /* Refer to Appendix A for available Value */
@@ -147,18 +149,18 @@ typedef struct message_0x29_t {
    } message_0x29_t;
   
   
-typedef struct message_0x2D_t { 
+typedef struct PACKED message_0x2D_t { 
     uint8_t  message_id ; /*  */
    } message_0x2D_t;
   
   
-typedef struct message_0x30_t { 
+typedef struct PACKED message_0x30_t { 
     uint8_t  message_id ; /*  */
     uint8_t  sv_ ; /* 0: means all SVs 1~32 : mean for the particular SV */
    } message_0x30_t;
   
   
-typedef struct message_0x31_t { 
+typedef struct PACKED message_0x31_t { 
     uint8_t  message_id ; /*  */
     uint16_t  sv_id ; /* Satellite id */
     uint8_t  subframedata_0_0 ; /* Eph data subframe 1 */
@@ -194,30 +196,30 @@ typedef struct message_0x31_t {
    } message_0x31_t;
   
   
-typedef struct message_0x37_t { 
+typedef struct PACKED message_0x37_t { 
     uint8_t  message_id ; /*  */
     uint8_t  enable ; /* 0: disable 1: enable */
     uint8_t  attributes ; /* 0: update to SRAM 1: update to both SRAM & FLASH */
    } message_0x37_t;
   
   
-typedef struct message_0x38_t { 
+typedef struct PACKED message_0x38_t { 
     uint8_t  message_id ; /*  */
    } message_0x38_t;
   
   
-typedef struct message_0x39_t { 
+typedef struct PACKED message_0x39_t { 
     uint8_t  message_id ; /*  */
     uint8_t  position_pinning ; /* 0: disable (default) 1: enable */
    } message_0x39_t;
   
   
-typedef struct message_0x3A_t { 
+typedef struct PACKED message_0x3A_t { 
     uint8_t  message_id ; /*  */
    } message_0x3A_t;
   
   
-typedef struct message_0x3B_t { 
+typedef struct PACKED message_0x3B_t { 
     uint8_t  message_id ; /*  */
     uint16_t  pinning_speed ; /*  */
     uint16_t  pinning_cnt ; /*  */
@@ -227,31 +229,31 @@ typedef struct message_0x3B_t {
    } message_0x3B_t;
   
   
-typedef struct message_0x3C_t { 
+typedef struct PACKED message_0x3C_t { 
     uint8_t  message_id ; /*  */
     uint8_t  navigation_mode ; /* 0: car 1: pedestrian */
     uint8_t  attributes ; /* 0: update to SRAM 1: update to both SRAM & FLASH */
    } message_0x3C_t;
   
   
-typedef struct message_0x3D_t { 
+typedef struct PACKED message_0x3D_t { 
     uint8_t  message_id ; /*  */
    } message_0x3D_t;
   
   
-typedef struct message_0x3E_t { 
+typedef struct PACKED message_0x3E_t { 
     uint8_t  message_id ; /*  */
     uint8_t  n1pps_mode ; /* 0: off 1: on when 3D fix 2: on when 1 SV */
     uint8_t  attributes ; /* 0: update to SRAM 1: update to both SRAM & FLASH */
    } message_0x3E_t;
   
   
-typedef struct message_0x3F_t { 
+typedef struct PACKED message_0x3F_t { 
     uint8_t  message_id ; /*  */
    } message_0x3F_t;
   
   
-typedef struct message_0x80_t { 
+typedef struct PACKED message_0x80_t { 
     uint8_t  message_id ; /*  */
     uint8_t  software_type ; /* 0: Reserved 1: System code */
     uint32_t  kernel_version ; /* X1.Y1.Z1 = SkyTraq Kernel Version Ex. X1=01, Y1=00, Z1=01 (1.0.1) */
@@ -260,32 +262,32 @@ typedef struct message_0x80_t {
    } message_0x80_t;
   
   
-typedef struct message_0x81_t { 
+typedef struct PACKED message_0x81_t { 
     uint8_t  message_id ; /*  */
     uint8_t  software_type ; /* 0: Reserved 1: System code */
     uint16_t  crc ; /* CRC value */
    } message_0x81_t;
   
   
-typedef struct message_0x83_t { 
+typedef struct PACKED message_0x83_t { 
     uint8_t  message_id ; /*  */
     uint8_t  ack_id ; /* Message ID of the request message */
    } message_0x83_t;
   
   
-typedef struct message_0x84_t { 
+typedef struct PACKED message_0x84_t { 
     uint8_t  message_id ; /*  */
     uint8_t  ack_id ; /* Message ID of the request message */
    } message_0x84_t;
   
   
-typedef struct message_0x86_t { 
+typedef struct PACKED message_0x86_t { 
     uint8_t  message_id ; /*  */
     uint8_t  update_rate ; /* 01: 1Hz */
    } message_0x86_t;
   
   
-typedef struct message_0xA8_t { 
+typedef struct PACKED message_0xA8_t { 
     uint8_t  message_id ; /*  */
     uint8_t  fix_mode ; /* Quality of fix 0: no fix 1: 2D 2: 3D 3: 3D+DGPS */
     uint8_t  number_of_sv_in_fix ; /* Number of SV in fix 0-12 */
@@ -309,13 +311,13 @@ typedef struct message_0xA8_t {
    } message_0xA8_t;
   
   
-typedef struct message_0xAE_t { 
+typedef struct PACKED message_0xAE_t { 
     uint8_t  message_id ; /*  */
     uint16_t  datum_index ; /* Datum index Refer to Appendix A & B */
    } message_0xAE_t;
   
   
-typedef struct message_0xB1_t { 
+typedef struct PACKED message_0xB1_t { 
     uint8_t  message_id ; /*  */
     uint16_t  sv_id ; /* Satellite id */
     uint8_t  subframedata_0_0 ; /* Eph data subframe 1 */
@@ -351,25 +353,25 @@ typedef struct message_0xB1_t {
    } message_0xB1_t;
   
   
-typedef struct message_0xB3_t { 
+typedef struct PACKED message_0xB3_t { 
     uint8_t  message_id ; /*  */
     uint8_t  waas_status ; /* 0: disable 1: enable */
    } message_0xB3_t;
   
   
-typedef struct message_0xB4_t { 
+typedef struct PACKED message_0xB4_t { 
     uint8_t  message_id ; /*  */
     uint8_t  status ; /* 0: disable 1: enable */
    } message_0xB4_t;
   
   
-typedef struct message_0xB5_t { 
+typedef struct PACKED message_0xB5_t { 
     uint8_t  message_id ; /*  */
     uint8_t  navigation_mode ; /* 0: car 1: pedestrian */
    } message_0xB5_t;
   
   
-typedef struct message_0xB6_t { 
+typedef struct PACKED message_0xB6_t { 
     uint8_t  message_id ; /*  */
     uint8_t  n1pps_mode ; /* 0: off 1: on */
    } message_0xB6_t;
